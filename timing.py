80b37f768d1391e2774ba39869cf22e867a1962e
Riley Chou <riley33chou@gmail.com>
2024-05-22 11:42:01 -0700

    big query

--- timing.py
+++ timing.py
@@ -4,7 +4,7 @@ import time
 
 t0 = time.time()
 # Create wiretrace object from vcd file:
-wiretrace = WireTrace.from_vcd("example/example1.vcd")
+wiretrace = WireTrace.from_vcd("example/example3.vcd")
 t1 = time.time()
 
 total = t1 - t0
@@ -12,8 +12,8 @@ print(f"Time taken: {total:.4f}s")
 
 t2 = time.time()
 # Convert wiretrace to svg:
-for i in range(0, 1000):
-    image = Visualizer(Style.Dark).to_svg(wiretrace, start=0, length=8, wires='D0, D1')
+for i in range(0, 10):
+    image = Visualizer(Style.Dark).to_svg(wiretrace, start=4, length=26, wires='clk,rst_n,clk,rst_n,pc,pc_next,inst,immed,wreg,wdata,sdata,tdata,op,alu_in1,alu_in2,alu_out,zero,rdata,alu_src,alu_shift,branch,mem_to_reg,mem_read,mem_write,reg_dst,reg_write,addr,rdata,clk,rst_n,reg_write,wreg,wdata,sdata,tdata')
 t3 = time.time()
 
 total2 = t3 - t2
