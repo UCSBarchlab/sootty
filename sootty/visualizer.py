401352b96fc94e5059f9fffceae270c479052d2e
Riley Chou <rileychou@ucsb.edu>
2024-05-29 15:32:24 -0700

    master copy timing

--- sootty/visualizer.py
+++ sootty/visualizer.py
@@ -98,7 +98,7 @@ class Visualizer:
                 )
             )
             t4 = time.time()
-            print(f"Time taken - compute wires: {t4 - t3:.8f}s")
+            # print(f"{t4 - t3:.8f}s")
 
         return VectorImage(
             self._wiretrace_to_svg(
@@ -176,7 +176,7 @@ class Visualizer:
         svg += "</svg>"
         t2 = time.time()
 
-        print(f"Time taken - compute wire visualizer: {t2 - t1:.8f}s")
+        print(f"{t2 - t1:.8f}")
         return svg
 
     def _timestamps_to_svg(self, left, top, start, length):
